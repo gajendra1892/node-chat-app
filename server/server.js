0ad4c2e36e5389e3ae8f598fce7cda68c3aebbf5
Gajendar <gajendra.kumar@3pillarglobal.com>
2018-09-10 22:23:20 +0530

    Connect and disconnect event done

--- server/server.js
+++ server/server.js
@@ -1,18 +1,43 @@
 const path =require('path');
+const http=require('http');
 const express =require('express');
+const socketIO =require('socket.io');
 
 const publicPath =path.join(__dirname ,'../public');
 
 
-
-
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
-
 var app=express();
+var server =http.createServer(app);
+var io=socketIO(server);
+
+
 app.use(express.static(publicPath)); //to make the middleware
 const port =process.env.PORT || 3000;
 
-app.listen(port,()=>{
-console.log(`CHAT APP is started on ${port}`);
+
+
+
+io.on('connection',(socket)=>{
+
+    console.log('New user connected');
+
+    socket.on('disconnect',()=>{
+        console.log(' User was disconnected');
+    });
 });
+
+server.listen(port,()=>{
+    console.log(`CHAT APP is started on ${port}`);
+    });
+
+
+
+
+
+
+
+
+
+// app.listen(port,()=>{
+// console.log(`CHAT APP is started on ${port}`);
+// });
