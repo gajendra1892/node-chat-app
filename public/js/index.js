0bd8ec628e2bc17d32dde396f8deb70c39fc67d5
Gajendar <gajendra.kumar@3pillarglobal.com>
2018-09-13 21:38:12 +0530

    Added socket.emit and listners

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+
+
+var socket = io();
+
+socket.on('connect',function(){
+    console.log('connected to server');
+
+    // socket.emit('createEmail',{
+
+    //     to:'jen@example.com',
+    //     text:'Hey.This is Andrew.'
+    // });
+
+    socket.emit('createMessage',{
+        to:'gajendra',
+        from:'rahul',
+        text:'ok'
+    })
+});
+
+socket.on('disconnect',function(){
+   console.log('disconnected from server');
+});
+
+// socket.on('newEmail',function(email){
+
+//     console.log('New Email',email);
+// });
+
+socket.on('newMess',function(msg){
+
+    console.log('Client recievemsg:',msg)
+});
--- server/server.js
+++ server/server.js
@@ -20,11 +20,39 @@ const port =process.env.PORT || 3000;
 io.on('connection',(socket)=>{
 
     console.log('New user connected');
+   
+   
+    // socket.emit('newEmail',{
+    //     from:'nike@example.com',
+    //     text:'Hey.What is going on.',
+    //     createdAt:123
+    // });
+
+    socket.emit('newMess',{
+
+        from:'Gajendra',
+        to:'Rahul',
+        text:'Work properly'
+    });
+
+    // socket.on('createEmail',(newEmail)=>{
+    // console.log('createEmail',newEmail);
+
+    // });
+
+    socket.on('createMessage',(msg)=>{
+        console.log('server recive message',msg);
+    
+        });
 
     socket.on('disconnect',()=>{
         console.log(' User was disconnected');
     });
 });
+
+
+
+
 
 server.listen(port,()=>{
     console.log(`CHAT APP is started on ${port}`);
